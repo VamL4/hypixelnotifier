3654429ca9780d6508d787115da74f8372cb49da
VamL4 <axd@gmail.com>
2021-06-21 12:53:03 +0300

    v0.2

--- main.js
+++ main.js
@@ -14,28 +14,42 @@ client.on("ready", () => {
 client.on('message', (msg) => {
     if (msg.content == "!run") {
         msg.author.send('running...')
+        msg.author.send('Checker.js ... [ON]')
         const interval = setInterval(() => {
             util.status('play.hypixel.net')
                 .then((response) => {
                     console.log(response.onlinePlayers);
-                    if (response.onlinePlayers >= 20) {
-                        // 324261688062771200, 737896996517707776
-                        const Aliav = client.users.cache.get(id).displayAvatarURL({ dynamic: true });
-                        const Aliname = client.users.cache.get(id).username;
+                    if (response.onlinePlayers >= 0) {
+                        // 324261688062771200, 413791604369784832
                         const Alimsg = `**يا عرص هايبكسل فتح!!  \n 
-        ${response.onlinePlayers} **`;
+                        ${response.onlinePlayers} **`;
 
                         let MEmb = new Discord.MessageEmbed()
                             .setColor('#FF5733')
                             .setTitle('**رسالة مهمة**')
                             .setDescription(Alimsg)
                             .setTimestamp()
-                            .setAuthor(Aliname, Aliav);
+                            .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }));
 
-                        client.users.cache.get(id).send(MEmb);
                         client.users.cache.get(id2).send(MEmb);
+                        client.users.cache.get(id).send(MEmb);
                     }
                 })
         }, 5000)
+    }
+
+    else if (msg.content == "!test") {
+        // 324261688062771200, 737896996517707776
+        const Alimsg = `**يا عرص هايبكسل فتح!!  \n  **`;
+
+        let MEmb = new Discord.MessageEmbed()
+            .setColor('#FF5733')
+            .setTitle('**رسالة مهمة**')
+            .setDescription(Alimsg)
+            .setTimestamp()
+            .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }));
+
+        client.users.cache.get(id2).send(MEmb);
+        client.users.cache.get(id).send(MEmb);
     }
 });
